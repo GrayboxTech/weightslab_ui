ae7ba01cb04d3e4d379ae15f86a9ad36c0d26111
AkanshaGraybox <akansha@graybx.com>
2025-05-28 17:12:49 +0200

    Optimized load_raw_data to work for both greyscale and RGB images

--- trainer_worker.py
+++ trainer_worker.py
@@ -20,7 +20,9 @@ from scope_timer import ScopeTimer
 from imagenet_exp import get_exp
 # from mnist_exp_fully_conv import get_exp
 
-experiment = get_exp()
+with ScopeTimer("fetching experiment") as t:
+    experiment = get_exp()
+print(t)
 experiment.set_is_training(True)
 
 
@@ -185,18 +187,23 @@ def load_raw_image(dataset, index):
     wrapped = dataset.wrapped_dataset if hasattr(dataset, "wrapped_dataset") else dataset
 
     if hasattr(wrapped, "data"):
-        # when image is stored in memory
         np_img = wrapped.data[index]
-        return Image.fromarray(np_img)
+        if hasattr(np_img, 'numpy'):
+            np_img = np_img.numpy()  
+        if np_img.ndim == 2:
+            return Image.fromarray(np_img.astype(np.uint8), mode="L")
+        elif np_img.ndim == 3:
+            return Image.fromarray(np_img.astype(np.uint8), mode="RGB")
+        else:
+            raise ValueError(f"Unsupported image shape: {np_img.shape}")
 
-    elif hasattr(wrapped, "samples"):
-        # when loading from a path
-        img_path, _ = wrapped.samples[index]
-        return Image.open(img_path).convert("RGB")
-
-    elif hasattr(wrapped, "imgs"):
-        img_path, _ = wrapped.imgs[index]
-        return Image.open(img_path).convert("RGB")
+    elif hasattr(wrapped, "samples") or hasattr(wrapped, "imgs"):
+        if hasattr(wrapped, "samples"):
+            img_path, _ = wrapped.samples[index]
+        else:
+            img_path, _ = wrapped.imgs[index]
+        img = Image.open(img_path)
+        return img.convert("L") if img.mode in ["1", "L", "I;16", "I"] else img.convert("RGB")
 
     else:
         raise ValueError("Dataset type not supported for raw image extraction.")
@@ -321,7 +328,9 @@ class ExperimentServiceServicer(pb2_grpc.ExperimentServiceServicer):
             return pb2.SampleRequestResponse(
                 error_message=f"Sample {request.sample_id} not found.")
 
-        transformed_tensor, idx, label = dataset._getitem_raw(request.sample_id)
+        with ScopeTimer('time: getiteam_raw') as t:
+            transformed_tensor, idx, label = dataset._getitem_raw(request.sample_id)
+        print(t)
         # #TODO: apply transform too
         
         transformed_image_bytes = tensor_to_bytes(transformed_tensor)
