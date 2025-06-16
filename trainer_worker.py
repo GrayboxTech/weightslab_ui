8da04fc7dce5096c513907aead296ed38275576d
Alexandru Rotaru <rotarualexandruandrei94@gmail.com>
2025-06-16 16:09:58 +0200

    Minor fixes and adjustement for trainer_procesS

--- trainer_worker.py
+++ trainer_worker.py
@@ -24,15 +24,14 @@ from fashion_mnist_exp import get_exp
 # from cad_models_exp import get_exp
 
 experiment = get_exp()
-experiment.set_is_training(True)
+# experiment.set_is_training(True)
 
 
 def training_thread_callback():
     while True:
-        # print("Training thread callback ", str(experiment), end="\r")
+        print("Training thread callback ", str(experiment), end="\r")
         if experiment.get_is_training():
             experiment.train_step_or_eval_full()
-            # print(f"[TRAIN] Steps left: {experiment.get_training_steps_to_do()}")
 
 
 training_thread = Thread(target=training_thread_callback)
