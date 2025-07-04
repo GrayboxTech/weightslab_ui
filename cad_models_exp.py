e1f427a03f746203f1ac9003ee51660a10dcdadd
Alexandru Rotaru <rotarualexandruandrei94@gmail.com>
2025-07-04 17:16:34 +0200

    Add cad_models

--- cad_models_exp.py
+++ cad_models_exp.py
@@ -87,6 +87,7 @@ class SmallCIFARNet(NetworkWithOps, nn.Module):
         return out
 
 
+<<<<<<< HEAD
 transform = T.Compose([T.Resize((224, 224)), T.ToTensor(),])
 root_dir = "/home/rotaru/Desktop/GRAYBOX/sales/pitch/prepare/cad_models_dataset_split/"
 train_dataset = ds.ImageFolder(
@@ -95,6 +96,30 @@ val_dataset = ds.ImageFolder(
     os.path.join(root_dir, "val"), transform=transform)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+=======
+transform = T.Compose([T.Resize((256, 256)), T.ToTensor(),])
+
+# Load dataset
+full_dataset = ds.ImageFolder(
+    root="/home/rotaru/Desktop/GRAYBOX/sales/pitch/prepare/cad_models_dataset/",
+    # "cad_models_dataset/",
+    transform=transform)
+
+# Calculate sizes for train and validation sets
+total_size = len(full_dataset)
+train_size = int(0.8 * total_size)
+val_size = total_size - train_size
+
+# Split the dataset
+train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
+train_dataset = train_dataset.dataset
+val_dataset = val_dataset.dataset
+
+# import pdb; pdb.set_trace()
+
+
+device = th.device("cuda:0")
+>>>>>>> 6634baf (Add cad_models_exp temporarily)
 
 def get_exp():
     model = SmallCIFARNet()
