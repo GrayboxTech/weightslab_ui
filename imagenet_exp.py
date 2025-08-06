f38ee63459b2f56f5aeba5523abbebc09bc229ee
AkanshaGraybox <akansha@graybx.com>
2025-08-06 11:05:54 +0200

    Latest exp for ec2

--- imagenet_exp.py
+++ imagenet_exp.py
@@ -17,126 +17,46 @@ from weightslab.tracking import TrackingMode, add_tracked_attrs_to_input_tensor
 
 from board import Dash
 
-class TinyImageNetNet(NetworkWithOps, nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.tracking_mode = TrackingMode.DISABLED
-
-        self.conv1 = Conv2dWithNeuronOps(3,   8, kernel_size=3, padding=1)
-        self.bn1   = BatchNorm2dWithNeuronOps(8)
-
-        self.conv2 = Conv2dWithNeuronOps(8, 16, kernel_size=3, padding=1)
-        self.bn2   = BatchNorm2dWithNeuronOps(16)
-
-        self.conv3 = Conv2dWithNeuronOps(16, 32, kernel_size=3, padding=1)
-        self.bn3   = BatchNorm2dWithNeuronOps(32)
-
-        self.conv4 = Conv2dWithNeuronOps(32, 64, kernel_size=3, padding=1)
-        self.bn4   = BatchNorm2dWithNeuronOps(64)
-
-        self.conv5 = Conv2dWithNeuronOps(64, 128, kernel_size=3, padding=1)
-        self.bn5   = BatchNorm2dWithNeuronOps(128)
-
-        self.pool = nn.MaxPool2d(2)                 # down-sample ×2
-        self.gap  = nn.AdaptiveAvgPool2d(1)         # 4×4 → 1×1
-        self.dropout = nn.Dropout(p=0.20)
-
-        self.fc1 = LinearWithNeuronOps(128, 128)
-        self.fc2 = LinearWithNeuronOps(128, 256)
-        self.fc3 = LinearWithNeuronOps(256, 200)
-
-        self.softmax = nn.Softmax(dim=1)
-
-
-    def children(self):
-        return [
-            self.conv1, self.bn1,
-            self.conv2, self.bn2,
-            self.conv3, self.bn3,
-            self.conv4, self.bn4,
-            self.conv5, self.bn5,
-            self.fc1, self.fc2, self.fc3
-        ]
-
-    def define_deps(self):
-        self.register_dependencies([
-            (self.conv1, self.bn1, DepType.SAME),
-            (self.bn1,   self.conv2, DepType.INCOMING),
-            (self.conv2, self.bn2, DepType.SAME),
-            (self.bn2,   self.conv3, DepType.INCOMING),
-            (self.conv3, self.bn3, DepType.SAME),
-            (self.bn3,   self.conv4, DepType.INCOMING),
-            (self.conv4, self.bn4, DepType.SAME),
-            (self.bn4,   self.conv5, DepType.INCOMING),
-            (self.conv5, self.bn5, DepType.SAME),
-            (self.bn5,   self.fc1,   DepType.INCOMING),
-            (self.fc1,   self.fc2,   DepType.INCOMING),
-            (self.fc2,   self.fc3,   DepType.INCOMING)
-        ])
-        self.flatten_conv_id = self.bn3.get_module_id()
-
-    def forward(self, x):
-        self.maybe_update_age(x)
-        x = self.pool(F.relu(self.bn1(self.conv1(x))))  # 64→32
-        x = self.pool(F.relu(self.bn2(self.conv2(x))))  # 32→16
-        x = self.pool(F.relu(self.bn3(self.conv3(x))))  # 16→8
-        x = self.pool(F.relu(self.bn4(self.conv4(x))))  # 8→4
-        x = self.pool(F.relu(self.bn5(self.conv5(x))))  # 4→2
-        x = self.gap(x)                                 # 2x2 → 1x1
-        x = x.view(x.size(0), -1)                       # (B, 128)
-        x = self.dropout(x)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-
-        output = self.fc3(x, skip_register=True, intermediary=None)
-
-        one_hot = F.one_hot(output.argmax(dim=1), num_classes=self.fc3.out_features)
-
-        if hasattr(x, 'in_id_batch') and hasattr(x, 'label_batch'):
-            add_tracked_attrs_to_input_tensor(
-                one_hot, in_id_batch=x.in_id_batch, label_batch=x.label_batch)
-
-        self.fc3.register(one_hot)
-        output = self.softmax(output)
-        return output
-    
-
-# class TinyImageNet_2(NetworkWithOps, nn.Module):
+# class TinyImageNetNet(NetworkWithOps, nn.Module):
 #     def __init__(self):
 #         super().__init__()
 #         self.tracking_mode = TrackingMode.DISABLED
 
-#         self.conv1 = Conv2dWithNeuronOps(3,   16, kernel_size=3, padding=1)
-#         self.bn1   = BatchNorm2dWithNeuronOps(16)
+#         self.conv1 = Conv2dWithNeuronOps(3,   8, kernel_size=3, padding=1)
+#         self.bn1   = BatchNorm2dWithNeuronOps(8)
 
-#         self.conv2 = Conv2dWithNeuronOps(16, 32, kernel_size=3, padding=1)
-#         self.bn2   = BatchNorm2dWithNeuronOps(32)
+#         self.conv2 = Conv2dWithNeuronOps(8, 16, kernel_size=3, padding=1)
+#         self.bn2   = BatchNorm2dWithNeuronOps(16)
 
-#         self.conv3 = Conv2dWithNeuronOps(32, 64, kernel_size=3, padding=1)
-#         self.bn3   = BatchNorm2dWithNeuronOps(64)
+#         self.conv3 = Conv2dWithNeuronOps(16, 32, kernel_size=3, padding=1)
+#         self.bn3   = BatchNorm2dWithNeuronOps(32)
 
-#         self.conv4 = Conv2dWithNeuronOps(64, 128, kernel_size=3, padding=1)
-#         self.bn4   = BatchNorm2dWithNeuronOps(128)
+#         self.conv4 = Conv2dWithNeuronOps(32, 64, kernel_size=3, padding=1)
+#         self.bn4   = BatchNorm2dWithNeuronOps(64)
 
-#         self.conv5 = Conv2dWithNeuronOps(128, 256, kernel_size=3, padding=1)
-#         self.bn5   = BatchNorm2dWithNeuronOps(256)
+#         self.conv5 = Conv2dWithNeuronOps(64, 128, kernel_size=3, padding=1)
+#         self.bn5   = BatchNorm2dWithNeuronOps(128)
 
-#         self.pool = nn.MaxPool2d(2)
-#         self.gap  = nn.AdaptiveAvgPool2d(1)
-#         self.dropout = nn.Dropout(p=0.3)
+#         self.pool = nn.MaxPool2d(2)                 # down-sample ×2
+#         self.gap  = nn.AdaptiveAvgPool2d(1)         # 4×4 → 1×1
+#         self.dropout = nn.Dropout(p=0.20)
 
-#         self.fc1 = LinearWithNeuronOps(256, 512)
-#         self.fc2 = LinearWithNeuronOps(512, 200)
+#         self.fc1 = LinearWithNeuronOps(128, 128)
+#         self.fc2 = LinearWithNeuronOps(128, 256)
+#         self.fc3 = LinearWithNeuronOps(256, 200)
 
 #         self.softmax = nn.Softmax(dim=1)
 
+
 #     def children(self):
-#         return [self.conv1, self.bn1,
-#                 self.conv2, self.bn2,
-#                 self.conv3, self.bn3,
-#                 self.conv4, self.bn4,
-#                 self.conv5, self.bn5,
-#                 self.fc1, self.fc2]
+#         return [
+#             self.conv1, self.bn1,
+#             self.conv2, self.bn2,
+#             self.conv3, self.bn3,
+#             self.conv4, self.bn4,
+#             self.conv5, self.bn5,
+#             self.fc1, self.fc2, self.fc3
+#         ]
 
 #     def define_deps(self):
 #         self.register_dependencies([
@@ -151,33 +71,113 @@ class TinyImageNetNet(NetworkWithOps, nn.Module):
 #             (self.conv5, self.bn5, DepType.SAME),
 #             (self.bn5,   self.fc1,   DepType.INCOMING),
 #             (self.fc1,   self.fc2,   DepType.INCOMING),
+#             (self.fc2,   self.fc3,   DepType.INCOMING)
 #         ])
 #         self.flatten_conv_id = self.bn3.get_module_id()
 
 #     def forward(self, x):
 #         self.maybe_update_age(x)
-#         for conv, bn in [(self.conv1, self.bn1),
-#                          (self.conv2, self.bn2),
-#                          (self.conv3, self.bn3),
-#                          (self.conv4, self.bn4),
-#                          (self.conv5, self.bn5)]:
-#             x = self.pool(F.relu(bn(conv(x))))
-#         x = self.gap(x)
-#         x = x.view(x.size(0), -1)
+#         x = self.pool(F.relu(self.bn1(self.conv1(x))))  # 64→32
+#         x = self.pool(F.relu(self.bn2(self.conv2(x))))  # 32→16
+#         x = self.pool(F.relu(self.bn3(self.conv3(x))))  # 16→8
+#         x = self.pool(F.relu(self.bn4(self.conv4(x))))  # 8→4
+#         x = self.pool(F.relu(self.bn5(self.conv5(x))))  # 4→2
+#         x = self.gap(x)                                 # 2x2 → 1x1
+#         x = x.view(x.size(0), -1)                       # (B, 128)
 #         x = self.dropout(x)
 #         x = F.relu(self.fc1(x))
-#         output = self.fc2(x, skip_register=True, intermediary=None)
+#         x = F.relu(self.fc2(x))
 
-#         one_hot = F.one_hot(output.argmax(dim=1), num_classes=self.fc2.out_features)
+#         output = self.fc3(x, skip_register=True, intermediary=None)
+
+#         one_hot = F.one_hot(output.argmax(dim=1), num_classes=self.fc3.out_features)
 
 #         if hasattr(x, 'in_id_batch') and hasattr(x, 'label_batch'):
 #             add_tracked_attrs_to_input_tensor(
 #                 one_hot, in_id_batch=x.in_id_batch, label_batch=x.label_batch)
 
-#         self.fc2.register(one_hot)
-
-
+#         self.fc3.register(one_hot)
+#         output = self.softmax(output)
 #         return output
+    
+
+class TinyImageNet_2(NetworkWithOps, nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.tracking_mode = TrackingMode.DISABLED
+
+        self.conv1 = Conv2dWithNeuronOps(3,   16, kernel_size=3, padding=1)
+        self.bn1   = BatchNorm2dWithNeuronOps(16)
+
+        self.conv2 = Conv2dWithNeuronOps(16, 32, kernel_size=3, padding=1)
+        self.bn2   = BatchNorm2dWithNeuronOps(32)
+
+        self.conv3 = Conv2dWithNeuronOps(32, 64, kernel_size=3, padding=1)
+        self.bn3   = BatchNorm2dWithNeuronOps(64)
+
+        self.conv4 = Conv2dWithNeuronOps(64, 128, kernel_size=3, padding=1)
+        self.bn4   = BatchNorm2dWithNeuronOps(128)
+
+        self.conv5 = Conv2dWithNeuronOps(128, 256, kernel_size=3, padding=1)
+        self.bn5   = BatchNorm2dWithNeuronOps(256)
+
+        self.pool = nn.MaxPool2d(2)
+        self.gap  = nn.AdaptiveAvgPool2d(1)
+        self.dropout = nn.Dropout(p=0.3)
+
+        self.fc1 = LinearWithNeuronOps(256, 512)
+        self.fc2 = LinearWithNeuronOps(512, 200)
+
+        self.softmax = nn.Softmax(dim=1)
+
+    def children(self):
+        return [self.conv1, self.bn1,
+                self.conv2, self.bn2,
+                self.conv3, self.bn3,
+                self.conv4, self.bn4,
+                self.conv5, self.bn5,
+                self.fc1, self.fc2]
+
+    def define_deps(self):
+        self.register_dependencies([
+            (self.conv1, self.bn1, DepType.SAME),
+            (self.bn1,   self.conv2, DepType.INCOMING),
+            (self.conv2, self.bn2, DepType.SAME),
+            (self.bn2,   self.conv3, DepType.INCOMING),
+            (self.conv3, self.bn3, DepType.SAME),
+            (self.bn3,   self.conv4, DepType.INCOMING),
+            (self.conv4, self.bn4, DepType.SAME),
+            (self.bn4,   self.conv5, DepType.INCOMING),
+            (self.conv5, self.bn5, DepType.SAME),
+            (self.bn5,   self.fc1,   DepType.INCOMING),
+            (self.fc1,   self.fc2,   DepType.INCOMING),
+        ])
+        self.flatten_conv_id = self.bn3.get_module_id()
+
+    def forward(self, x):
+        self.maybe_update_age(x)
+        for conv, bn in [(self.conv1, self.bn1),
+                         (self.conv2, self.bn2),
+                         (self.conv3, self.bn3),
+                         (self.conv4, self.bn4),
+                         (self.conv5, self.bn5)]:
+            x = self.pool(F.relu(bn(conv(x))))
+        x = self.gap(x)
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        x = F.relu(self.fc1(x))
+        output = self.fc2(x, skip_register=True, intermediary=None)
+
+        one_hot = F.one_hot(output.argmax(dim=1), num_classes=self.fc2.out_features)
+
+        if hasattr(x, 'in_id_batch') and hasattr(x, 'label_batch'):
+            add_tracked_attrs_to_input_tensor(
+                one_hot, in_id_batch=x.in_id_batch, label_batch=x.label_batch)
+
+        self.fc2.register(one_hot)
+
+
+        return output
 
 # Normalization
 mean = [0.485, 0.456, 0.406]
@@ -217,7 +217,7 @@ metrics = {
 
 
 def get_exp():
-    model = TinyImageNetNet()
+    model = TinyImageNet_2()
     model.define_deps()
     model.to(device)
 
@@ -233,8 +233,8 @@ def get_exp():
         metrics=metrics,
         training_steps_to_do=30000,
         name="v0",
-        root_log_dir="tinyimagenet-exp1_ckpt",
-        logger=Dash("tinyimagenet-exp1_ckpt"),
+        root_log_dir="tinyimagenet-exp3_ckpt",
+        logger=Dash("tinyimagenet-exp3_ckpt"),
         skip_loading=False
     )
 
